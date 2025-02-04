8754ab73e610e12bbe51e7a8ff314771e1a4653e
dotcom07 <dotcombubblecom@gmail.com>
2025-02-05 01:36:35 +0900

    2

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { io } from "socket.io-client"
 
 // Components
 import Navigation from './components/Navigation'
-import Servers from './components/Servers'
 import Channels from './components/Channels'
+import Servers from './components/Servers'
 import Messages from './components/Messages'
 
 // ABIs
@@ -19,9 +19,37 @@ const socket = io('ws://localhost:3030');
 
 function App() {
 
+  const [provider, setProvider] = useState("");
   const [account, setAccount] = useState("");
+  const [dappcord, setDappcord] = useState();
+  const [channels, setChannels] = useState([]);
+
+  const [currentChannel, setCurrentChannel] = useState(null);
+
+  const [messages, setMessages] = useState([]);
 
   const loadBlockchainData = async () => {
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+
+    const network = await provider.getNetwork();
+    const dappcord = new ethers.Contract( config[network.chainId].Dappcord.address , Dappcord , provider )
+    setDappcord(dappcord);
+
+    const totalChannels = await dappcord.totalChannels();
+
+    let channels = [];
+
+    for (let i = 0 ; i<totalChannels ; i++) {
+      const channel = await dappcord.getChannel(i+1);
+      channels.push(channel);
+    }
+
+    setChannels(channels);
+
+    
 
     window.ethereum.on('accountsChanged', async () => {
       window.location.reload();
@@ -31,12 +59,40 @@ function App() {
 
   useEffect(()=>{
     loadBlockchainData();
+
+    socket.on("connect", ()=>{
+      socket.emit("get messages")
+      console.log("connected")
+    })
+  
+    socket.on("new message", (messages)=>{
+      setMessages(messages);
+    })
+  
+    socket.on("get messages", (messages)=>{
+      setMessages(messages);
+    })
+
+    return () => {
+      socket.off("connect")
+      socket.off("new message")
+      socket.off("get message")
+    }
   },[])
+
+
+
+  
 
   return (
     <div>
        <Navigation account={account} setAccount={setAccount}/>
+      
       <main>
+
+        <Servers/>
+        <Channels provider={provider} account={account} dappcord={dappcord} channels={channels} currentChannel={currentChannel} setCurrentChannel={setCurrentChannel}/>
+        <Messages account={account} messages={messages} currentChannel={currentChannel}/>
 
       </main>
     </div>
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -10,9 +10,35 @@ const socket = io('ws://localhost:3030')
 
 const Messages = ({ account, messages, currentChannel }) => {
 
+  console.log(currentChannel);
+
+
+  const showMessage = () => {
+    if (!currentChannel) {
+      console.error("currentChannel is null or undefined");
+    } else {
+      console.log("채널 선택됨");
+      return messages.filter(message => message.channel === currentChannel.id.toString())
+        .map((message, index) => (
+          <div className='message' key={index}>
+              <img src={person} alt="Person"/>
+              <div className='message_content'>
+                <h3>{message.account.slice(0,6)+"..."+message.account.slice(38,42)}</h3>
+                <p>
+                {message.text}
+                </p>
+              </div>
+            </div> 
+        ));
+    }
+  };
+
   return (
     <div className="text">
       <div className="messages">
+        
+
+      {showMessage()}
 
       </div>
 
