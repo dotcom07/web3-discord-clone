98e67127a5e8549d1525ad80ab49cf3815ea6eba
dotcom07 <dotcombubblecom@gmail.com>
2025-02-04 23:28:17 +0900

    Initial commit with files

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,6 +6,33 @@ const tokens = (n) => {
 
 async function main() {
 
+  let deployer;
+  let dappcord;
+
+  const NAME = "Dappcord";
+  const SYMBOL = "DC";
+
+  [deployer] = await ethers.getSigners();
+
+  // Deploy contract
+  const Dappcord = await ethers.getContractFactory("Dappcord");
+  dappcord = await Dappcord.deploy(NAME, SYMBOL);
+  await dappcord.deployed(); //배포가 완료될 때까지 대기
+
+  console.log("contract : ", dappcord.address)
+
+  const CHANNEL_NAMES = ["general","intro","jobs"]
+  const COSTS = [tokens(1),tokens(0.5),tokens(0.25)]
+
+  for (let i = 0; i<3; i++){
+  // Create a channel
+  const transaction = await dappcord.connect(deployer).createChannel(CHANNEL_NAMES[i], COSTS[i]);
+  await transaction.wait();
+
+  console.log("생성 완료",CHANNEL_NAMES[i]);
+  }
+
+
 }
 
 main().catch((error) => {
--- src/App.js
+++ src/App.js
@@ -19,10 +19,23 @@ const socket = io('ws://localhost:3030');
 
 function App() {
 
+  const [account, setAccount] = useState("");
+
+  const loadBlockchainData = async () => {
+
+    window.ethereum.on('accountsChanged', async () => {
+      window.location.reload();
+    })
+
+  }
+
+  useEffect(()=>{
+    loadBlockchainData();
+  },[])
+
   return (
     <div>
-      <h1 style={{ textAlign: "center", padding: "15px" }}>Welcome to Dappcord</h1>
-
+       <Navigation account={account} setAccount={setAccount}/>
       <main>
 
       </main>
